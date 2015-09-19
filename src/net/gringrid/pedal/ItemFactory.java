fb09498a9c050da570e2fed3d786829b70074d09
choijiho <choijiho@choijihoui-MacBook-Pro.local>
2015-09-19 13:37:38 +0900

    mac gg

--- src/net/gringrid/pedal/ItemFactory.java
+++ src/net/gringrid/pedal/ItemFactory.java
@@ -1,10 +1,22 @@
 package net.gringrid.pedal;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.content.Context;
 import android.view.View;
 
 public abstract class ItemFactory {
+
+	private static final Map<String, ItemFactory> factoryMap = 
+	    Collections.unmodifiableMap(new HashMap<String, ItemFactory>() {{
+	        put("Meow", new ItemFactory() { public Animal create() { return new Cat(); }});
+	        put("Woof", new ItemFactory() { public Animal create() { return new Dog(); }});
+	    }});
+
 	public final View create(String itemName, Context context){
+
 		View view = createView(itemName);
 		return view;
 	}

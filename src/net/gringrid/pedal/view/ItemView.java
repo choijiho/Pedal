4a012a1bedc9b6b31e4e13e3262ffc7b256d97ec
choijiho <choijiho.korea@gmail.com>
2015-09-25 16:31:32 +0900

    gg

--- src/net/gringrid/pedal/view/ItemView.java
+++ src/net/gringrid/pedal/view/ItemView.java
@@ -2,9 +2,13 @@ package net.gringrid.pedal.view;
 
 import net.gringrid.pedal.R;
 import net.gringrid.pedal.db.vo.DisplayVO;
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -65,43 +69,74 @@ public class ItemView extends FrameLayout{
 		mTitleView.setText(mVo.itemName);
 		mTitleView.setTextSize(mVo.titleFontSize);
 		mTitleView.setTag(mVo);
-		mTitleView.setBackgroundResource(R.drawable.item_unselected);
+//		mTitleView.setBackgroundResource(R.drawable.item_unselected);
 		mTitleView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 	}
 	
 	private void drawContentView(){
+		Display display = ((Activity)mContext).getWindowManager().getDefaultDisplay(); 
+		int screenWidth = display.getWidth();
+		int screenHeight = display.getHeight();
+		int itemWidth = mVo.right - mVo.left;
+		int itemHeight = mVo.bottom - mVo.top;
+		
+		float densityWidth = (float)itemWidth / screenWidth;
+		float densityHeight = (float)itemHeight / screenHeight;
+		float contentTextSize = densityHeight * 240;
+		float unitTextSize = densityHeight * 45;
+		float additionalWidth = densityWidth / 10;
+		
+		Log.d("jiho", "screenWidth : "+screenWidth);
+		Log.d("jiho", "screenHeight: "+screenHeight);
+		Log.d("jiho", "densityWidth : "+densityWidth);
+		Log.d("jiho", "densityHeight : "+densityHeight);
+		Log.d("jiho", "contentTextSize : "+contentTextSize);
+		Log.d("jiho", "unitTextSize : "+unitTextSize);
+		Log.d("jiho", "(int)(densityHeight*100) : "+(int)(densityHeight*100));
+		Log.d("jiho", "additionalWidth : "+additionalWidth);
+		
 		mContentLayout = new LinearLayout(mContext);
 		mContentLayout.setOrientation(LinearLayout.HORIZONTAL);
         mContentLayout.setBackgroundResource(R.drawable.item_unselected);
-		mContentLayout.setLayoutParams(new android.widget.LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 1f));
+        mContentLayout.setPadding(0, 0, 0, 0);
+		mContentLayout.setLayoutParams(new android.widget.LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+		mContentLayout.setGravity(Gravity.BOTTOM);
 		
 		mContentView = new TextView(mContext);
 		mContentView.setText("00.0");
-		mContentView.setTextSize(mVo.contentFontSize);
-		mContentView.setBackgroundResource(R.drawable.item_unselected);
-		mContentView.setLayoutParams(new android.widget.LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT, 7f));
+		mContentView.setTextSize(TypedValue.COMPLEX_UNIT_SP, contentTextSize);
+		mContentView.setBackgroundResource(R.drawable.item_selected);
+		mContentView.setPadding(0, 0, 0, 0);
+		mContentView.setGravity(Gravity.BOTTOM | Gravity.RIGHT);
+		mContentView.setLayoutParams(new android.widget.LinearLayout.LayoutParams((int)(itemWidth * (0.7f+additionalWidth)), LayoutParams.WRAP_CONTENT));
 		
 		mUnitView = new TextView(mContext);
 		mUnitView.setText("km/h");
-		mUnitView.setTextSize(mVo.unitFontSize);
-        mUnitView.setBackgroundResource(R.drawable.item_unselected);
-		mUnitView.setLayoutParams(new android.widget.LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT, 3f));
-		
+		mUnitView.setTextSize(TypedValue.COMPLEX_UNIT_SP, unitTextSize);
+        mUnitView.setBackgroundResource(R.drawable.item_selected);
+        mUnitView.setPadding(0, 0, 0, 0);
+        mUnitView.setGravity(Gravity.BOTTOM | Gravity.LEFT);
+		mUnitView.setLayoutParams(new android.widget.LinearLayout.LayoutParams((int)(itemWidth * (1-(0.7f+additionalWidth))), LayoutParams.WRAP_CONTENT));
+	
 		mContentLayout.addView(mContentView);
 		mContentLayout.addView(mUnitView);
 	}
 	
 	private void doAssemble(){
+		
 		Log.d("jiho", "doAssemble");
 		int itemWidth = mVo.right - mVo.left;
 		int itemHeight = mVo.bottom - mVo.top;
+
 		mVo.params = new FrameLayout.LayoutParams(itemWidth, itemHeight);
 		mVo.params.setMargins(mVo.left, mVo.top, 0, 0);
-
+		
         Log.d("jiho", "itemwidth : " + itemWidth);
-        mContentView.setTextSize((float) (itemWidth / 5.5));
-        mUnitView.setTextSize((float) (itemWidth / 17));
-
+        LinearLayout marginTop = new LinearLayout(mContext);
+        marginTop.setLayoutParams(new android.widget.LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 1f));
+        LinearLayout marginBottom = new LinearLayout(mContext);
+        marginBottom.setLayoutParams(new android.widget.LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 1f));
+      
 		mLayout.addView(mTitleView);
 		mLayout.addView(mContentLayout);
 		
